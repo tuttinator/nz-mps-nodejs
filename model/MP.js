8511aeed8632213501ed851a71649639efb3f163
Caleb Tutty <caleb@prettymint.co.nz>
2014-07-24 23:25:56 +1200

    Started an MP model

---
+++ model/MP.js
@@ -0,0 +1,9 @@
+function MP(name, link, electorateDetails) {
+  this.name = name;
+  this.link = link;
+  this.electorateDetails = electorateDetails;
+}
+
+MP.prototype.extractItems = function() {
+  
+}
